22e396d0a7e893f851b6b0e3ef6c1cfc9700aa4b
Drew <andrew.althage@gmail.com>
2021-06-30 18:01:38 -0600

    trigger build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { i18n } = require('./next-i18next.config');
 module.exports = {
     i18n,
     env: {
-        apiBaseUrl: process.env.API_BASE_URL
+        apiBaseUrl: process.env.API_BASE_URL,
+        build: 'trigger-build'
     },
 };
