37645fef8655c9bfd4cd3ae378ca54b99fb04598
Drew <andrew.althage@gmail.com>
2021-07-01 17:31:57 -0600

    mobile menu wip

--- components/global/primary-navigation.tsx
+++ components/global/primary-navigation.tsx
@@ -5,23 +5,23 @@ import { useAppDispatch, useAppSelector } from 'store'
 import {
   selectUserData,
   setUser,
-  setUserError,
   setUserLoading,
 } from 'store/slices/user.slice'
 import { authRefresh } from 'controllers'
 import { useEffect } from 'react'
+import { selectAppWindowWidth, setWidth } from '../../store/slices/app.slice'
 
 const PrimaryNavigation = () => {
   const dispatch = useAppDispatch()
   const user = useAppSelector(selectUserData)
+  const windowWidth = useAppSelector(selectAppWindowWidth)
+
   const refreshUser = async () => {
     try {
       dispatch(setUserLoading(true))
       const result = await authRefresh()
       dispatch(setUser(result))
     } catch (e) {
-      console.log('e', e)
-      dispatch(setUserError(true))
     } finally {
       dispatch(setUserLoading(false))
     }
@@ -29,59 +29,69 @@ const PrimaryNavigation = () => {
 
   useEffect(() => {
     refreshUser()
+    dispatch(setWidth(window.innerWidth))
+    window.addEventListener('resize', () =>
+        dispatch(setWidth(window.innerWidth))
+    )
   }, [])
 
   return (
-    <Layout.Header
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-      }}
-    >
-      <div style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}>
-        <div style={{ height: 38 }}>
+    <>
+      <Layout.Header
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div
+          style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}
+        >
+          <div style={{ height: 38 }}>
+            <Link href="/">
+              <a>
+                <Image src="/img/wh2o-logo.svg" height={38} width={67} />
+              </a>
+            </Link>
+          </div>
           <Link href="/">
             <a>
-              <Image src="/img/wh2o-logo.svg" height={38} width={67} />
+              <Typography.Title
+                level={3}
+                style={{ color: '#fff', marginBottom: 0, marginLeft: 12 }}
+              >
+                wh2o
+              </Typography.Title>
             </a>
           </Link>
         </div>
-        <Link href="/">
-          <a>
-            <Typography.Title
-              level={3}
-              style={{ color: '#fff', marginBottom: 0, marginLeft: 12 }}
-            >
-              wh2o
-            </Typography.Title>
-          </a>
-        </Link>
-      </div>
-      <Menu theme="dark" mode="horizontal">
-        <Menu.Item key="1">
-          <Link href="/rivers">
-            <a>Rivers</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Link href="/gages">
-            <a>Gages</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="3">
-          {!user.email ? (
-            <Link key="login" href="/auth/login">
-              <a>Sign In</a>
-            </Link>
-          ) : (
-            <Link key="dashboard" href={`/user/${user.id}`}>
-              <a>Dashboard</a>
-            </Link>
-          )}
-        </Menu.Item>
-      </Menu>
-    </Layout.Header>
+        {windowWidth > 768 && (
+          <Menu theme="dark" mode="horizontal">
+            <Menu.Item key="1">
+              <Link href="/rivers">
+                <a>Rivers</a>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Link href="/gages">
+                <a>Gages</a>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="3">
+              {!user.email ? (
+                <Link key="login" href="/auth/login">
+                  <a>Sign In</a>
+                </Link>
+              ) : (
+                <Link key="dashboard" href={`/user/${user.id}`}>
+                  <a>Dashboard</a>
+                </Link>
+              )}
+            </Menu.Item>
+          </Menu>
+        )}
+      </Layout.Header>
+    </>
   )
 }
 
