118ec3d8ecc0a62f2ae2cd2ed30e592f31dac03f
Drew <andrew.althage@gmail.com>
2022-05-03 22:20:12 -0600

    chore: unauthorized page updates

--- src/pages/Auth/Unauthorized.tsx
+++ src/pages/Auth/Unauthorized.tsx
@@ -10,9 +10,19 @@ export const Unauthorized = () => {
         title="403"
         subTitle="Sorry, you are not authorized to access this page."
         extra={
-          <Button onClick={() => navigate('/')} type="primary">
-            Back Home
-          </Button>
+          <>
+            <Button
+              onClick={() => navigate('/auth/login', { replace: true })}
+              type="primary"
+            >
+              Sign In
+            </Button>
+            <Button
+              onClick={() => navigate('/auth/register', { replace: true })}
+            >
+              Sign Up
+            </Button>
+          </>
         }
       />
     </div>
