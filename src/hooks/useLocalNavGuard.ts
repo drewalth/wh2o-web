cb890efc130079fe007b3222c84f3675c563aed9
Drew <andrew.althage@gmail.com>
2022-05-03 22:19:38 -0600

    fix: local nav guard hook

--- src/hooks/useLocalNavGuard.ts
+++ src/hooks/useLocalNavGuard.ts
@@ -1,6 +1,7 @@
 import { useUserContext } from '../components/User/UserContext'
 import { useNavigate } from 'react-router-dom'
 import { useLayoutEffect } from 'react'
+import { getToken } from '../lib/token'
 
 export type UseLocalNavGuardOptions = {
   redirectPath?: string
@@ -17,22 +18,30 @@ const defaultOptions: UseLocalNavGuardOptions = {
  * check to see if user is logged in, if not redirect.
  */
 export const useLocalNavGuard = (input?: UseLocalNavGuardOptions) => {
-  const options = { ...defaultOptions, ...input }
+  const options: UseLocalNavGuardOptions = { ...defaultOptions, ...input }
   const { redirectPath, checkIsAdmin } = options
   const { user, requestStatus } = useUserContext()
   const navigate = useNavigate()
 
   useLayoutEffect(() => {
-    if (!user && requestStatus !== 'loading' && !!redirectPath) {
+    const token = getToken()
+
+    if (
+      (!token && !!redirectPath) ||
+      (!user && requestStatus !== 'loading' && !!redirectPath)
+    ) {
       navigate(redirectPath, { replace: true })
+      return
     }
     if (requestStatus === 'failure' && !user) {
       navigate('/five-hundred', { replace: true })
+      return
     }
 
     if (user && requestStatus === 'success' && checkIsAdmin && !!redirectPath) {
       if (!['ADMIN', 'SUPERADMIN'].includes(user.role)) {
         navigate(redirectPath, { replace: true })
+        return
       }
     }
   }, [requestStatus])
