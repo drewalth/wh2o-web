c8fcab37133804dd2a1ddf8241a0c7c8dcc08720
Drew <andrew.althage@gmail.com>
2022-05-08 21:21:48 -0600

    build: temp disable service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 import './locale/i18n'
 const isProduction = process.env.NODE_ENV === 'production'
@@ -18,8 +18,11 @@ ReactDOM.render(
   document.getElementById('root'),
 )
 
-if (isProduction) {
-  serviceWorkerRegistration.register()
-} else {
-  serviceWorkerRegistration.unregister()
-}
+/**
+ * @temp service worker is kinda of annoying rn... enable later...
+ */
+// if (isProduction) {
+//   serviceWorkerRegistration.register()
+// } else {
+//   serviceWorkerRegistration.unregister()
+// }
