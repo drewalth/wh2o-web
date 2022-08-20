d4a93640d5d44543e99f3c0ff72cb4d8bbd255f9
Drew <andrew.althage@gmail.com>
2022-08-20 10:31:22 -0600

    build: service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration'
-// import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 import './locale/i18n'
-// const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === 'production'
 
-// if (isProduction) {
-//   disableReactDevTools()
-// }
+if (isProduction) {
+  disableReactDevTools()
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,8 +18,9 @@ ReactDOM.render(
   document.getElementById('root'),
 )
 
-// if (isProduction) {
-//   serviceWorkerRegistration.register()
-// } else {
-// serviceWorkerRegistration.unregister()
-// }
+if (isProduction) {
+  console.log('Registering service worker')
+  serviceWorkerRegistration.register()
+} else {
+  serviceWorkerRegistration.unregister()
+}
